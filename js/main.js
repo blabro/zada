cd8ea5dff44643a4879585fae27c85809cd27da7
blabro <blabro@o2.pl>
2016-12-08 11:31:25 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  $(document).on('mouseenter', '.great', function () {
-console.log('wjezdza');
-      $(this).find(":button").show();
+  $("li").on('mouseover', '.great', function () {
+      $("button[type=kupbutton]").show();
   }).on('mouseleave', '.great', function () {
-console.log('wyezdza');
-      $(this).find(":button").hide();
+      $("button[type=kupbutton]").hide();
+  });
+
+  var i = 0, sum = 0, suma=0;
+  $("button[type=kupbutton]").mousedown(function () {
+    i++;
+    suma += parseFloat(this.value);
+    sum = suma.toFixed(2);
+    document.getElementById('koszyk').innerHTML = '(' + i + ')' + sum + ' ZŁ';
+    console.log( '(' + i + ')' + sum + ' ZŁ');
   });
 });
